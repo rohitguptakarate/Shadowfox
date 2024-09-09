5da54e48767dc96918f67d7aa65d8eadb202a9f4
rohitguptakarate <rohitguptakarategzp@gmail.com>
2024-09-09 21:17:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+document.getElementById('whatsappIcon').addEventListener('click',function(){
+    // window.location.href="https://wa.me/9198734788?text=Hello Rohit sir";
+    window.open("https://wa.me/9198734788?text=Hello_Rohit_sir", "_blank");
+
+});
+
+document.getElementById('instagramIcon').addEventListener('click',function(){
+    window.open("https://instagram.com/rohi1432", "_blank");
+    // window.location.href="https://instagram.com/rohi1432"
+})
+
+document.getElementById('emailIcon').addEventListener('click',function(){
+    window.location.href="mailto:rohitguptakarategzp@gmail.com"
+})
